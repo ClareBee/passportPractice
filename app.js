ea9d685ed1f67c47fb760eadcbe9c91f3a263b3f
Clare <chblackburne@gmail.com>
2018-01-07 15:43:47 +0000

    Adds in passport session  and express session to app.js

--- app.js
+++ app.js
@@ -2,13 +2,23 @@ var   express = require("express"),
       mongoose = require("mongoose"),
       passport = require("passport"),
       bodyParser = require("body-parser"),
+      User = require("./models/user"),
       localStrategy = require("passport-local"),
       passportLocalMongoose = require("passport-local-mongoose");
 
 mongoose.connect("mongodb://localhost/authdemoapp");
-var app = express();
 
+var app = express();
 app.set('view engine', 'ejs');
+
+app.use(require("express-session")({
+  secret: 'Open sesame',
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.get('/', function(req, res){
   res.render("home");
