1287c6121a295381b3ab5e713405ddf706f924d7
Clare <chblackburne@gmail.com>
2018-01-07 16:04:23 +0000

    User registration added with 'local' strategy

--- app.js
+++ app.js
@@ -16,7 +16,9 @@ app.use(require("express-session")({
   resave: false,
   saveUninitialized: false
 }));
-
+//we need this line every time we set up a form
+app.use(bodyParser.urlencoded({extended: true}));
+//sets up passport session
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -39,6 +41,23 @@ app.get('/secret', function(req, res){
 //show sign-up form
 app.get("/register", function(req, res){
   res.render("register");
+});
+//convention to call this the same as the GET route
+app.post("/register", function(req, res){
+  req.body.username
+  req.body.password
+  //don't pass in the password into the database but pass it as a second argument to .register() which hashes it
+  User.register(new User({username: req.body.username}), req.body.password, function(err, user){
+    if(err){
+      console.log(err);
+      return res.render("register");
+    }
+      //log the user in and store the info/runs serialize methods - "local" is the name of the strategy - could be "twitter" etc
+    passport.authenticate("local")(req, res, function(){
+        res.redirect("/secret");
+      });
+  });
+
 });
 app.listen("3000", function(){
   console.log("server started");
